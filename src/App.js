1c55cb2577aef38d480cdf5011c59bd5be338f8e
onggi <hsk1533@naver.com>
2022-04-19 17:11:41 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import "./css/App.css";
+import MainScene from "./scene/mainScene";
 
 function App() {
-  return <div className="App">init</div>;
+  return (
+    <div className="App">
+      <MainScene />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/scene/mainScene.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainScene() {
+  return (
+    <div className="main-scene">
+      <canvas className="main-scene-canvas"></canvas>
+    </div>
+  );
+}
+
+export default MainScene;
